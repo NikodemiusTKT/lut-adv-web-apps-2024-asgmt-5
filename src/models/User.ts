1f61bc3b73fb0546f522ec2d1851d77e8693508f
Teemu Tanninen <teemuxtanninen@gmail.com>
2024-12-02 18:02:05 +0200

    feat: add user model

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface ITodo {
+  todo: string;
+  checked: boolean;
+}
+
+interface IUser extends Document {
+  name: string;
+  todos: ITodo[];
+}
+
+const TodoSchema = new Schema<ITodo>({
+  todo: { type: String, required: true },
+  checked: { type: Boolean, default: false },
+});
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  todos: [TodoSchema],
+});
+
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export { User, IUser, ITodo };
